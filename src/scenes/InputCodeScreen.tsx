d117e974c8bf3969db292ed46ada47c2332a13e1
Quang Việt <20522153@ms.uit.edu.vn>
2022-12-20 20:27:40 +0700

    add plain text

--- src/scenes/InputCodeScreen.tsx
+++ src/scenes/InputCodeScreen.tsx
@@ -6,15 +6,17 @@ import { Heading1, Heading2, PlainText } from "components/CustomizedText";
 type Props = {};
 
 const InputCodeScreen = (props: Props) => {
-	return (
-		<BaseView>
-			<PlainText>Enter your code provide by the organizerrr</PlainText>
-			<TextInput className="border" placeholder="Hello" />
-			<TouchableOpacity>
-				<PlainText>Oke</PlainText>
-			</TouchableOpacity>
-		</BaseView>
-	);
+  return (
+    <BaseView>
+      <PlainText>Enter your code provide by the organizerrr</PlainText>
+      <PlainText>Enter your code provide by the organizerrr</PlainText>
+
+      <TextInput className="border" placeholder="Hello" />
+      <TouchableOpacity>
+        <PlainText>Oke</PlainText>
+      </TouchableOpacity>
+    </BaseView>
+  );
 };
 
 export default InputCodeScreen;
